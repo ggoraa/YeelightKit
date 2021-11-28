18903557d0c103d55546e1581e238c922a6543c1
ggoraa <ggoraa1029@gmail.com>
2021-11-28 16:50:42 +0200

    added region enum

---
+++ Sources/YeeLampa/Region.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Егор Яковенко on 28.11.2021.
+//
+
+public enum Region {
+	case Germany
+	
+	var urlFormat: String {
+		switch self {
+			case .Germany: return "de"
+		}
+	}
+}
