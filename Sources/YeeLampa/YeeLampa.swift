774dc025fd1db42b674dfb6d88a82c9ff9ed0a4c
ggoraa <ggoraa1029@gmail.com>
2021-11-28 12:18:57 +0200

    Started implementing main class of YeeLampa

--- Sources/YeeLampa/YeeLampa.swift
+++ Sources/YeeLampa/YeeLampa.swift
@@ -1,6 +1,22 @@
-public struct YeeLampa {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
+import Foundation
+/// Main class of YeeLampa.
+public class YeeLampa {
+	private let clientId = "2882303761517308695"
+	private let clientSecret = "OrwZHJ/drEXakH1LsfwwqQ=="
+	private let region: Region
+	private let accessToken: String
+	private let deviceListUrl = URL(string: "https://de.openapp.io.mi.com/openapp/user/device_list")
+	
+	/// Creates a YeeLampa instance with provided credentials.
+	/// - Parameters:
+	/// 	- accessToken: The token you acquired when logging in.
+	/// 	- region: The region we will connect to.
+	public init(accessToken: String, region: Region) {
+		self.region = region
+		self.accessToken = accessToken
+	}
+	
+	public enum Region {
+		case De
+	}
 }
