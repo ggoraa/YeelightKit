691e2a96b5a48d5fe747520b76a4fb7d61243d23
ggoraa <ggoraa1029@gmail.com>
2021-11-28 16:51:10 +0200

    started work on setup logic

--- Sources/YeeLampa/YeeLampa.swift
+++ Sources/YeeLampa/YeeLampa.swift
@@ -5,18 +5,38 @@ public class YeeLampa {
 	private let clientSecret = "OrwZHJ/drEXakH1LsfwwqQ=="
 	private let region: Region
 	private let accessToken: String
-	private let deviceListUrl = URL(string: "https://de.openapp.io.mi.com/openapp/user/device_list")
+	private let deviceListUrl = URL(string: "https://\(region.urlFormat).openapp.io.mi.com/openapp/user/device_list")
 	
 	/// Creates a YeeLampa instance with provided credentials.
 	/// - Parameters:
 	/// 	- accessToken: The token you acquired when logging in.
 	/// 	- region: The region we will connect to.
-	public init(accessToken: String, region: Region) {
+	public init(accessToken: String, region: Region) throws {
 		self.region = region
 		self.accessToken = accessToken
-	}
-	
-	public enum Region {
-		case De
+		var request = URLRequest(url: deviceListUrl!)
+		request.httpMethod = "GET"
+		request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+		let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+
+			// Check if Error took place
+			if let error = error {
+				throw InitError.logInFailure(description: error.text)
+			}
+
+			// Read HTTP Response Status code
+			if let response = response as? HTTPURLResponse {
+				if (response.statusCode != 200) {
+					throw InitError.logInFailure(description: "Bad status code received from server: \(response.statusCode)")
+				}
+			}
+
+			// Convert HTTP Response Data to a simple String
+			if let data = data, let dataString = String(data: data, encoding: .utf8) {
+				print("Response data string:\n \(dataString)")
+			}
+
+		}
+		task.resume()
 	}
 }
