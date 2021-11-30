28be50f6b6f9b590cae10d5b898c29c0efd2dbde
ggoraa <ggoraa1029@gmail.com>
2021-11-30 21:18:16 +0200

    finished init and deviceList returning logic

--- Sources/YeeLampa/YeeLampa.swift
+++ Sources/YeeLampa/YeeLampa.swift
@@ -1,11 +1,12 @@
 import Foundation
+
 /// Main class of YeeLampa.
 public class YeeLampa {
-	private let clientId = "2882303761517308695"
+	private let clientId = 2882303761517308695
 	private let clientSecret = "OrwZHJ/drEXakH1LsfwwqQ=="
-	private let region: Region
-	private let accessToken: String
-	private let deviceListUrl = URL(string: "https://\(region.urlFormat).openapp.io.mi.com/openapp/user/device_list")
+	private var region: Region
+	private var accessToken: String
+	private let deviceListUrl: URL
 	
 	/// Creates a YeeLampa instance with provided credentials.
 	/// - Parameters:
@@ -14,29 +15,38 @@ public class YeeLampa {
 	public init(accessToken: String, region: Region) throws {
 		self.region = region
 		self.accessToken = accessToken
-		var request = URLRequest(url: deviceListUrl!)
-		request.httpMethod = "GET"
+		self.deviceListUrl = URL(string: "https://\(region.urlFormat).openapp.io.mi.com/openapp/user/device_list")!
+		print(self.deviceListUrl)
+	}
+	
+	/// Returns the device list.
+	/// This function does not use a cache, it returns new data from the server.
+	public func getDeviceList(completion: @escaping (Result<[Device], Error>) -> Void) {
+		var request = URLRequest(url: self.deviceListUrl)
+		request.httpMethod = "POST"
 		request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-		let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-
-			// Check if Error took place
-			if let error = error {
-				throw InitError.logInFailure(description: error.text)
+		request.httpBody = Data("clientId=\(self.clientId)&accessToken=\(self.accessToken)".utf8)
+		let requestTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
+			print("in task")
+			guard error == nil else {
+				print("\(String(describing: error))")
+				return completion(.failure(RequestError.badToken))
 			}
-
-			// Read HTTP Response Status code
+			
 			if let response = response as? HTTPURLResponse {
+				print(response.statusCode)
 				if (response.statusCode != 200) {
-					throw InitError.logInFailure(description: "Bad status code received from server: \(response.statusCode)")
+					print("Bad status code received from server: \(response.statusCode)")
 				}
 			}
 
 			// Convert HTTP Response Data to a simple String
-			if let data = data, let dataString = String(data: data, encoding: .utf8) {
-				print("Response data string:\n \(dataString)")
+			guard data == nil else {
+				print(String(data: data!, encoding: .utf8)!)
+				let parsedData = try! JSONDecoder().decode(DeviceListJsonModel.self, from: data!)
+				return completion(.success(parsedData.result.list))
 			}
-
 		}
-		task.resume()
+		requestTask.resume()
 	}
 }
