a1bda3e6afda9a72f6438744cd4be9283dd46203
ggoraa <ggoraa1029@gmail.com>
2021-12-01 11:01:15 +0200

    added DeviceModel enum and a String extension to convert to it

---
+++ Sources/YeeLampa/DeviceModel.swift
@@ -0,0 +1,19 @@
+//
+//  DeviceModel.swift
+//  
+//
+//  Created by Егор Яковенко on 30.11.2021.
+//
+
+public enum DeviceModel: Decodable {
+	case LightStrip(Int, String)
+	case DeskLamp(Int, String)
+	case BedsideLamp(Int, String)
+	case CeilingLamp(Int, String)
+}
+
+extension String {
+	func toDeviceModel() -> DeviceModel {
+		return DeviceModel.LightStrip(1, "yeelink.light.strip4")
+	}
+}
