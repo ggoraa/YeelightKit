98fa183e38150d4d9f939ec21c4527b2ad3f32d1
ggoraa <ggoraa1029@gmail.com>
2021-12-01 11:01:44 +0200

    added a lot of new fields, separated internal and public structs for representing a device

--- Sources/YeeLampa/Models/DeviceListModel.swift
+++ Sources/YeeLampa/Models/DeviceListModel.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  DeviceListModel.swift
 //  
 //
 //  Created by Егор Яковенко on 30.11.2021.
@@ -7,14 +7,32 @@
 
 struct DeviceListJsonModel: Decodable {
 	struct Results: Decodable {
-		let list: [Device]
+		struct DeviceJsonModel: Decodable {
+			let did: String
+			let name: String
+			let desc: String
+			let model: String
+			let mac: String
+			let longitude: Double
+			let latitude: Double
+			let isOnline: Bool
+			
+			func toPDevice() -> Device {
+				return Device(deviceId: self.did, name: self.name, description: self.desc, model: self.model.toDeviceModel(), macAddress: self.mac, longitude: self.longitude, latitude: self.latitude, isOnline: self.isOnline)
+			}
+		}
+		let list: [DeviceJsonModel]
 	}
 	let result: Results
 }
 
 public struct Device: Decodable {
-	let did: String
+	let deviceId: String
 	let name: String
-	let model: String
-	let mac: String
+	let description: String
+	let model: DeviceModel
+	let macAddress: String
+	let longitude: Double
+	let latitude: Double
+	let isOnline: Bool
 }
