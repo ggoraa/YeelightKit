be8944e6476c4a93872154d2f9de590502c7527f
ggoraa <ggoraa1029@gmail.com>
2021-12-18 21:10:42 +0200

    completely rewritten API: - now it uses Swifts new concurrency APIs (async await) - replaced URLSession and Requester usages with Alamofire - refactored toggle(device:on:): now it's setPower(to:for)

--- Sources/YeeLampa/YeeLampa.swift
+++ Sources/YeeLampa/YeeLampa.swift
@@ -19,67 +19,86 @@ public class YeeLampa {
 		self.deviceListUrl = URL(string: "https://\(region.urlFormat).openapp.io.mi.com/openapp/user/device_list")!
 	}
 	
+	private struct AuthResponse: Codable {
+		let access_token: String
+	}
+	
+	/// Converts an authorization grant to a usable access token.
+	/// - Parameters:
+	/// 	- grant: A grant to convert.
+	public static func convertAuthGrantToToken(_ grant: String) async throws -> String {
+		let url = URL(string: "https://account.xiaomi.com/oauth2/token")!
+		let task = AF.request(
+			url,
+			method: .get,
+			parameters: [
+				"client_id": YeeLampa.clientId,
+				"client_secret": YeeLampa.clientSecret,
+				"grant_type": "authorization_code",
+				"redirect_uri": "http://www.mi.com",
+				"code": grant
+			],
+			headers: [
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4495.0 Safari/537.36"
+			]
+		).serializingString()
+		do {
+			let response = try await task.value
+			let validJson = response.replacingOccurrences(of: "&&&START&&&", with: "")
+			return try! JSONDecoder().decode(AuthResponse.self, from: Data(validJson.utf8)).access_token
+		} catch {
+			throw LoginError.unknownError
+		}
+	}
+	
 	/// Returns the device list.
 	/// This function does not use a cache, it returns new data from the server.
-	public func getDeviceList(completion: @escaping (Result<[Device], Error>) -> Void) {
+	public func getDeviceList() async throws -> [Device] {
 		var request = URLRequest(url: self.deviceListUrl)
 		request.httpMethod = "POST"
 		request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-		request.httpBody = Data("clientId=\(self.clientId)&accessToken=\(self.accessToken)".utf8)
-		let requestTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
-			guard error == nil else {
-				return completion(.failure(RequestError.badToken))
-			}
-			
-			if let response = response as? HTTPURLResponse {
-				if (response.statusCode != 200) {
-					return completion(.failure(RequestError.unexpectedResponseCode(response.statusCode)))
-				}
-			}
-
-			// Convert HTTP Response Data to a simple String
-			guard data == nil else {
-				let parsedData = try! JSONDecoder().decode(BaseJsonModel<DeviceListJsonModel>.self, from: data!)
-				return completion(.success(parsedData.result.list.map { device in
-					return device.toPDevice()
-				}))
-			}
-		}
-		requestTask.resume()
-	}
-	
-	public func toggle(device: Device, on: Bool) {
-		var param = ""
-		if on {
-			param = "on"
-		} else {
-			param = "off"
-		}
-		self.sendRequestTo(
-			device: device,
-			arguments: ["method": "set_power", "params": [param]],
-			url: URL(string: "https://\(self.region.urlFormat).openapp.io.mi.com/openapp/device/rpc/\(device.deviceId)")!
-		) { result in
-			
+		request.httpBody = Data("clientId=\(YeeLampa.clientId)&accessToken=\(self.accessToken)".utf8)
+		
+		let task = AF.request(self.deviceListUrl, method: .post, parameters: [
+			"clientId": YeeLampa.clientId,
+			"accessToken": self.accessToken
+		], headers: [
+			"Content-Type": "application/x-www-form-urlencoded"
+		]).serializingDecodable(BaseJsonModel<DeviceListJsonModel>.self)
+		return try await task.value.result.list.map { device in
+			return device.toPDevice()
 		}
 	}
 	
-	private func sendRequestTo(device: Device, arguments: [String: Any], url: URL, _ completion: @escaping (Result<[Device], Error>) -> Void) {
+	public func setPower(to on: Bool, for device: Device) async throws {
+		try await self.sendDeviceRequest(
+			url: URL(string: "https://\(self.region.urlFormat).openapp.io.mi.com/openapp/device/rpc/\(device.deviceId)")!,
+			arguments: ["method": "set_power", "params": [on ? "on" : "off"]]
+		)
+	}
+	
+	/// An internal function to pass requests to devices.
+	/// - Parameters:
+	///   - url: A url to call when making a request.
+	///   - arguments: JSON arguments that will be stuffid in the request.
+	/// - Returns: A response in Data type.
+	private func sendDeviceRequest(url: URL, arguments: [String: Any]) async throws -> Data {
 		do {
-			let payload = try JSONSerialization.data(withJSONObject: arguments, options: .prettyPrinted)
-			Requester.shared.sendDataRequest(
-				url: url,
+			let payload = try JSONSerialization.data(withJSONObject: arguments)
+			let task = AF.request(
+				url,
 				method: .post,
-				body: "clientId=\(self.clientId)&accessToken=\(self.accessToken)&data=\(String(data: payload, encoding: .utf8)!.replacingOccurrences(of: "\n", with: ""))",
-				headers: ["Content-Type": "application/x-www-form-urlencoded"],
-				completion: { (_: String) in
-					
-				}, failure: { error in
-					return completion(.failure(RequestError.badToken))
-				}
-			)
+				parameters: [
+					"clientId": YeeLampa.clientId,
+					"accessToken": self.accessToken,
+					"data": String(data: payload, encoding: .utf8)!.replacingOccurrences(of: "\n", with: "")
+				],
+				headers: ["Content-Type": "application/x-www-form-urlencoded"]
+			).serializingData()
+			print(String(data: (try await task.value), encoding: .utf8))
+			return try await task.value
 		} catch {
-			print(error.localizedDescription)
+			throw RequestError.badToken
 		}
 	}
 }
