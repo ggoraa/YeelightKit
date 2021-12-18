50f69f4a530301f1b1d33367698febd297ffcaa7
ggoraa <ggoraa1029@gmail.com>
2021-12-18 21:11:19 +0200

    converted a struct to a class

--- Sources/YeeLampa/Models/Device.swift
+++ Sources/YeeLampa/Models/Device.swift
@@ -5,14 +5,30 @@
 //  Created by Егор Яковенко on 30.11.2021.
 //
 
-public struct Device: Hashable, Equatable {
-	public let deviceId: String
-	public let name: String
-	public let description: String
-	public let model: DeviceModel
-	public let macAddress: String
-	public let longitude: Double
-	public let latitude: Double
-	public let isOnline: Bool
-	public let isOn: Bool
+import Foundation
+
+open class Device: Identifiable {
+	public static func == (lhs: Device, rhs: Device) -> Bool {
+		return lhs.deviceId == rhs.deviceId
+	}
+	
+	let deviceId: String
+	let name: String
+	let model: DeviceModel
+	let macAddress: String
+	let longitude: Double
+	let latitude: Double
+	let isOnline: Bool
+	let isOn: Bool
+	
+	init(deviceId: String, name: String, model: DeviceModel, macAddress: String, longitude: Double, latitude: Double, isOnline: Bool, isOn: Bool) {
+		self.deviceId = deviceId
+		self.name = name
+		self.model = model
+		self.macAddress = macAddress
+		self.longitude = longitude
+		self.latitude = latitude
+		self.isOnline = isOnline
+		self.isOn = isOn
+	}
 }
