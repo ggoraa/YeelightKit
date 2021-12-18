2a24c5fa33c3a096c812ed6e7c521bdaadcc7295
ggoraa <ggoraa1029@gmail.com>
2021-12-18 21:05:04 +0200

    removed Requester dependency, added Alamofire as a replacement

--- Package.swift
+++ Package.swift
@@ -1,31 +1,31 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "YeeLampa",
+	name: "YeeLampa",
 	platforms: [
-		.iOS(.v9),
-		.macOS(.v10_10)
+		.iOS(.v13),
+		.macOS(.v10_15)
 	],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "YeeLampa",
-            targets: ["YeeLampa"]),
-    ],
-    dependencies: [
-		.package(url: "https://github.com/ILYA2606/Requester.git", revision: "1.0.6")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "YeeLampa",
-			dependencies: ["Requester"]),
-        .testTarget(
-            name: "YeeLampaTests",
-            dependencies: ["YeeLampa"]),
-    ]
+	products: [
+		.library(
+			name: "YeeLampa",
+			targets: ["YeeLampa"]),
+	],
+	dependencies: [
+		.package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.5.0"))
+	],
+	targets: [
+		.target(
+			name: "YeeLampa",
+			dependencies: ["Alamofire"],
+			resources: [
+				.copy("DeviceProps.plist")
+			]
+		),
+//		.testTarget(
+//			name: "YeeLampaTests",
+//			dependencies: ["YeeLampa"]),
+	]
 )
