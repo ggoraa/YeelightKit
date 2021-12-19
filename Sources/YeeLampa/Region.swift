615fca24c4c157162d1a45c22e4a179906a2414a
ggoraa <ggoraa1029@gmail.com>
2021-12-19 14:21:37 +0200

    compacted Region enum

--- Sources/YeeLampa/Region.swift
+++ Sources/YeeLampa/Region.swift
@@ -5,14 +5,7 @@
 //  Created by Егор Яковенко on 28.11.2021.
 //
 
-public enum Region {
-	case Germany
-	case Russia
-	
-	var urlFormat: String {
-		switch self {
-			case .Germany: return "de"
-			case .Russia: return "ru"
-		}
-	}
+public enum Region: String {
+	case Germany = "de"
+	case Russia = "ru"
 }
